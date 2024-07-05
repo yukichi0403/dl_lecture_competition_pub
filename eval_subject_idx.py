5be701601c2eb29e140c1fcefb9b3f2570face19
yukichi0403 <bamboovalleyyuki@gmail.com>
2024-07-05 10:19:04 +0900

    modify eval loop

--- eval_subject_idx.py
+++ eval_subject_idx.py
@@ -15,6 +15,7 @@ from src.utils import *
 
 import shutil
 import gc
+from torch.utils.data import DataLoader, ConcatDataset, Subset
 
 
 @torch.no_grad()
@@ -27,30 +28,34 @@ def run(args: DictConfig):
     #    Dataloader
     # ------------------    
     test_set = ThingsMEGDataset("test", args.data_dir)
-    test_loader = torch.utils.data.DataLoader(
-        test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
-    )
+    subject_idxs_test = torch.load(os.path.join(args.data_dir, "test_subject_idxs.pt"))
+    unique_subjects = torch.unique(subject_idxs_test)
+    preds = np.zeros(len(test_set))
 
-    folds_preds = []
-    for fold in range(args.num_splits):
-        print(f"Fold {fold+1}/{args.num_splits}")
+    for subject in unique_subjects:
+        print(f"Subject {subject}")
+        test_indices = (subject_idxs_test == subject).nonzero(as_tuple=True)[0]
+        test_subset = Subset(test_set, test_indices)
+        test_loader = DataLoader(
+            test_subset, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
+        )
+
         model = CustomModel(
             args.backbone,
             args.num_classes
         ).to(args.device)
-        model.load_state_dict(torch.load(os.path.join(args.model_path, f"model_best_{fold}.pt"), map_location=args.device))
+        model.load_state_dict(torch.load(os.path.join(args.model_path, f"model_best_{subject}.pt"), map_location=args.device))
         
         model.eval()
-        preds = [] 
+        subject_preds = [] 
         for X, subject_idxs in tqdm(test_loader, desc="Validation"):        
-            preds.append(model(X.to(args.device)).detach().cpu())
+            subject_preds.append(model(X.to(args.device)).detach().cpu())
         
-        preds = torch.cat(preds, dim=0).numpy()
-        folds_preds.append(preds)
-    
-    final_preds = np.mean(folds_preds, axis=0)
-    np.save(os.path.join(savedir, "submission.npy"), final_preds)
-    cprint(f"Submission {final_preds.shape} saved at {savedir}", "cyan")
+        subject_preds = torch.cat(subject_preds, dim=0).numpy()
+        preds[test_indices] = subject_preds
+
+    np.save(os.path.join(savedir, "submission.npy"), preds)
+    cprint(f"Submission {preds.shape} saved at {savedir}", "cyan")
 
     # Google Driveにファイルをコピー
     drive_dir = os.path.join(args.data_dir, f"{args.expname}_{args.ver}")
@@ -60,11 +65,6 @@ def run(args: DictConfig):
     if os.path.exists(submission_path):
         shutil.copy(submission_path, drive_dir)
         print(f'Submission file saved to Google Drive: {drive_dir}')
-
-    model_path = os.path.join(savedir, f"model_best_{fold}.pt")
-    if os.path.exists(model_path):
-        shutil.copy(model_path, drive_dir)
-        print(f'Model saved to Google Drive: {drive_dir}')
 
 if __name__ == "__main__":
     run()
--- main_v2_EfficientNet_foldbysubject_idx.py
+++ main_v2_EfficientNet_foldbysubject_idx.py
@@ -160,36 +160,34 @@ def run(args: DictConfig):
     
     # ----------------------------------
     #  Start evaluation with best model
-    # ----------------------------------    
-    del train_loader, val_loader, train_subsampler, val_subsampler
-    gc.collect()
-    
+    # ----------------------------------        
     print("Now Loading Test Datasets")
     test_set = ThingsMEGDataset("test", args.data_dir)
-    test_loader = torch.utils.data.DataLoader(
-        test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
-    )
+    subject_idxs_test = torch.load(os.path.join(args.data_dir, "test_subject_idxs.pt"))
+    
+    preds = np.zeros(len(test_set))
+    for subject in unique_subjects:
+        print(f"Subject {subject}")
+        test_indices = (subject_idxs_test == subject).nonzero(as_tuple=True)[0]
+        test_subset = Subset(test_set, test_indices)
+        test_loader = DataLoader(test_subset, shuffle=False, **loader_args)
 
-    folds_preds = []
-    for fold in range(args.num_splits):
-        print(f"Fold {fold+1}/{args.num_splits}")
         model = CustomModel(
             args.backbone,
             args.num_classes
         ).to(args.device)
-        model.load_state_dict(torch.load(os.path.join(args.model_path, f"model_best_{fold}.pt"), map_location=args.device))
+        model.load_state_dict(torch.load(os.path.join(args.model_path, f"model_best_{subject}.pt"), map_location=args.device))
         
         model.eval()
-        preds = [] 
+        subject_preds = [] 
         for X, subject_idxs in tqdm(test_loader, desc="Validation"):        
-            preds.append(model(X.to(args.device)).detach().cpu())
+            subject_preds.append(model(X.to(args.device)).detach().cpu())
         
-        preds = torch.cat(preds, dim=0).numpy()
-        folds_preds.append(preds)
-    
-    final_preds = np.mean(folds_preds, axis=0)
-    np.save(os.path.join(logdir, "submission.npy"), final_preds)
-    cprint(f"Submission {final_preds.shape} saved at {logdir}", "cyan")
+        subject_preds = torch.cat(subject_preds, dim=0).numpy()
+        preds[test_indices] = subject_preds
+
+    np.save(os.path.join(logdir, "submission.npy"), preds)
+    cprint(f"Submission {preds.shape} saved at {logdir}", "cyan")
 
     # Google Driveにファイルをコピー
     drive_dir = os.path.join(args.data_dir, f"{args.expname}_{args.ver}")
@@ -199,11 +197,6 @@ def run(args: DictConfig):
     if os.path.exists(submission_path):
         shutil.copy(submission_path, drive_dir)
         print(f'Submission file saved to Google Drive: {drive_dir}')
-
-    model_path = os.path.join(logdir, f"model_best_{fold}.pt")
-    if os.path.exists(model_path):
-        shutil.copy(model_path, drive_dir)
-        print(f'Model saved to Google Drive: {drive_dir}')
 
 
 
